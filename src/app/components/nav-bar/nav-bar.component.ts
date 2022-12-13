3a30d723aae6d18585cf6e64c1074b4fefcca669
mbmedeiros <mirelly_medeiros_@hotmail.com>
2022-12-13 16:20:47 -0300

    Implementando task42 - dark mode

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,12 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  public theme: string = "light-theme" //
+
   constructor() { }
 
   ngOnInit(): void {
+    let theme = localStorage.getItem("theme")
+    theme != null ? this.theme = theme : this.theme 
   }
 
   public logout(): void {
     // LOGOUT
   }
+
+  @HostBinding('class')
+  public get themeMode(): string {
+   return this.theme == "light-theme" ? "light-theme" : "dark-theme"
+
+  }
+ public switchTheme(): void{
+   this.theme == "light-theme" ? this.theme = "dark-theme" : this.theme = "light-theme"
+   localStorage.setItem("theme", this.theme)
+}
+
+
+  
 }
